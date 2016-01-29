36a28724f3636bd96c248c4758c1c76f4a234400
Alex <Alex@Alex-Vaio>
2016-01-29 12:43:59 +0100

    inizializzazione

---
+++ preventivi_serrature.php
@@ -0,0 +1,24 @@
+<?php
+
+//Autore: Alex Vezzelli - Alex Soluzioni Web
+//url: http://www.alexsoluzioniweb.it/
+ /**
+ * @package my_admin_area
+ */
+/*
+Plugin Name: My Admin Area
+Plugin URI: 
+Description: Plugin che genera un'area riservata personalizzata
+Version: 1.0
+Author: Alex Vezzelli - Alex Soluzioni Web
+Author URI: http://www.alexsoluzioniweb.it/
+License: GPLv2 or later
+*/
+
+
+//indico la cartella dove è contenuto il plugin
+require_once (dirname(__FILE__) . '/prevenitiv_serrature.php');
+
+
+
+?>
